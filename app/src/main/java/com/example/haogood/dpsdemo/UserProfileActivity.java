8cd2a481f0a006c86ce477585feb2eac7aa1a0a5
Haogood <ben831001@gmail.com>
2016-12-14 11:41:03 +0800

    get gps location and show latitude and longitude on textview

--- app/src/main/java/com/example/haogood/dpsdemo/UserProfileActivity.java
+++ app/src/main/java/com/example/haogood/dpsdemo/UserProfileActivity.java
@@ -1,11 +1,16 @@
 package com.example.haogood.dpsdemo;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +26,7 @@ public class UserProfileActivity extends AppCompatActivity {
     private LocationManager locationManager;
     private LocationListener locationListener;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +36,15 @@ public class UserProfileActivity extends AppCompatActivity {
         userName = (TextView) findViewById(R.id.userName);
         userName.setText(firebaseAuth.getCurrentUser().getEmail());
 
+        latitude = (TextView) findViewById(R.id.latitude);
+        longitude = (TextView)findViewById(R.id.longitude);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                latitude.append("" + location.getLatitude());
+                longitude.append("" + location.getLongitude());
             }
 
             @Override
@@ -49,26 +59,28 @@ public class UserProfileActivity extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String provider) {
-
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
             }
         };
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            requestPermissions(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+            }, 10);
             return;
+        }else{
+            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
         }
-        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode){
+//            case 10:
+//                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+//
+//                return;
+//        }
+//    }
 
     public void btnLogoutOnClick(View view){
         firebaseAuth.signOut();
